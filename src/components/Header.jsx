475ed532d6a4b05763816de35c1c58ba443e9946
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2023-01-22 21:58:57 +0100

    add ui for auth and header

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Realtime Chat App Made with React &amp; Firebase</h1>
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDigNb70G0dnD04v5Gm_pULgUn8Ngo-R6M",
+  authDomain: "realtime-chat-app-108d0.firebaseapp.com",
+  projectId: "realtime-chat-app-108d0",
+  storageBucket: "realtime-chat-app-108d0.appspot.com",
+  messagingSenderId: "483714238501",
+  appId: "1:483714238501:web:f0b2f0ce6c9e94e8dcd7cc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
